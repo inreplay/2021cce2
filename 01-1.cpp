31a27eb7411df751a45e920ebf3423e226a95fcc
inreplay <74502605+inreplay@users.noreply.github.com>
2022-02-24 17:44:58 +0800

    Add files via upload

---
+++ 01-1.cpp
@@ -0,0 +1,9 @@
+///123
+#include<stdio.h>
+int main(){
+    int a;
+    printf("讀入:");
+    scanf("%d" , &a);
+    printf("輸出 %d\n",a);
+    return 0;
+}
---
+++ 01-4.cpp
@@ -0,0 +1,19 @@
+#include<stdio.h>
+#include<string.h>
+
+char line[2000];
+int main(){
+	int aa=0;
+	for(int i=0;i<4;i++){
+            scanf("%s",line );
+			int n=strlen(line);
+			int a=0;
+			for(int i=0;i<n;i++){
+				if(line[i]=='2')a++;
+			}
+			printf("%d\n",a);aa+=a;
+	}
+	printf("Total: %d\n",aa);
+
+return 0;
+}
